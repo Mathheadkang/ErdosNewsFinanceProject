f7e006d4693f590c703c3db6f6e9839d3b9f04c5
XW Peng <26990979+xpeng-26@users.noreply.github.com>
2024-11-29 11:38:27 -0700

    update HDP to all_news data

--- src/engine/model_news/news_model_driver.py
+++ src/engine/model_news/news_model_driver.py
@@ -2,6 +2,7 @@
 import os
 import ast
 import pandas as pd
+from joblib import Parallel, delayed
 
 from gensim.corpora.dictionary import Dictionary
 from gensim.models import LdaMulticore, HdpModel
@@ -9,20 +10,36 @@ from gensim.models.coherencemodel import CoherenceModel
 
 
 def topic_model_driver(config, logger):
-		# Load the data
-		data = pd.read_csv(
-			os.path.join(
-				config['info']['local_data_path'],
-				"data_clean",
-				config['news_model']['input']['news_data_file']
+		if len(config['news_model']['input']['news_data_file']) > 1:
+			# Load the data
+			dataset = config['news_model']['input']['news_data_file']
+			df = []
+			for file in dataset:
+				df.append(pd.read_csv(
+					os.path.join(
+						config['info']['local_data_path'],
+						"data_clean/All_news_years",
+						file
+					)
+				))
+			data = pd.concat(df, ignore_index=True)
+		else:
+			# Load the data
+			data = pd.read_csv(
+				os.path.join(
+					config['info']['local_data_path'],
+					"data_clean",
+					config['news_model']['input']['news_data_file']
+				)
 			)
-		)
 
 		# Select the columns
 		data = data[["date", "token"]]
 		data = data.sort_values(by="date")
 		data = data.reset_index(drop=True)
 		data['token'] = data['token'].apply(lambda row: [str(token) for token in ast.literal_eval(row)])
+
+		logger.info(f"Loaded data in shape: {data.shape}")
 
 		# train, validation, test data split
 		data_train = data[
@@ -39,22 +56,25 @@ def topic_model_driver(config, logger):
 		corpus_train = [dict.doc2bow(doc) for doc in data_train["token"]]
 
 		# Train the model
+		logger.info(f"Training the model with {config['news_model']['method']} method...")
 		if 'LDA' in config['news_model']['method']:
 			model = LdaMulticore(
 				corpus = corpus_train,
 				id2word = dict,
 				iterations = 100,
-				num_topics = config['news_model']['lda_topics'],
+				num_topics = config['news_model']['topics'],
 				workers = config['news_model']['lda_num_cores'],
 				passes = 100
 			)
 		elif 'HDP' in config['news_model']['method']:
-			hdp_model = HdpModel(corpus = corpus_train, id2word = dict)
+			hdp_model = HdpModel(corpus = corpus_train, id2word = dict, T=config['news_model']['topics'])
 			model = hdp_model.suggested_lda_model()
 		else:
 			raise ValueError("The method is not supported.")
-		
+		logger.info(f"Model training is done.")
+
 		# Compute the coherence score of topics
+		logger.info(f"Computing the coherence score of topics...")
 		coherence_model = CoherenceModel(
 			model = model,
 			texts = data_train["token"],
@@ -68,12 +88,21 @@ def topic_model_driver(config, logger):
 		
 
 		# Predict the topics on all data
+		logger.info(f"Predicting the topics on all data (training and testing)...")
 		corpus = [dict.doc2bow(doc) for doc in data["token"]]
 		topics_inference = model.get_document_topics(corpus)
-		data['topic'] = [sorted(model[corpus][text])[0][0] for text in range(len(topics_inference))]
+		def sorted_topics(topics):
+			return sorted(topics, key=lambda x: x[1], reverse=True)
+		topics_inference_ = Parallel(n_jobs=-1)(delayed(sorted_topics)(topics) for topics in topics_inference)
+
+		data['key_topic'] = [topics[0][0] for topics in topics_inference_]
+		data['key_topic_wights'] = [topics[0][1] for topics in topics_inference_]
+		data['all_topics'] = topics_inference_
+		logger.info(f"Prediction is done.")
 
 		# Save the data
-		data.to_csv(
+		data_output = data[["date", "key_topic", 'key_topic_wights', 'all_topics']]
+		data_output.to_csv(
 			os.path.join(
 				config['info']['local_data_path'],
 				"model_news",
@@ -83,19 +112,26 @@ def topic_model_driver(config, logger):
 		)
 
 		# Save the model
-		model.save(
-			os.path.join(
-				config['info']['local_data_path'],
-				"model_news",
-				config['news_model']['method'] + "_" + str(num_topics) + "_" + config['news_model']['output']['model_file']
-			)
+		model_path = os.path.join(
+			config['info']['local_data_path'],
+			"model_news",
+			config['news_model']['method'] + "_" + str(num_topics) + "_" + config['news_model']['output']['model_file']
 		)
+		model.save(model_path)
+		# model = HdpModel.load(model_path)
 
 		# Save the topics
+		topn = config['news_model']['topn']
+		topic_words = [model.show_topic(topicid, topn=topn) for topicid in range(num_topics)]
+		topic_words = [[
+			[word for word, _ in topic],
+			[round(wt, 6) for _, wt in topic]
+		]	for topic in topic_words]
 		topics = pd.DataFrame(
 			{
 				"topic": range(num_topics),
-				"words": [model.show_topic(topicid, topn=20) for topicid in range(num_topics)]
+				"words": [topic[0] for topic in topic_words],
+				"weights": [topic[1] for topic in topic_words]
 			}
 		)
 		topics.to_csv(
@@ -108,4 +144,4 @@ def topic_model_driver(config, logger):
 		)
 
 
-		return model
+		return None
